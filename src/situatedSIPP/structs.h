7600af18c885ea14771525357dc4cb2e57fbfd08
Devin Thomas <devin.w.thomas@gmail.com>
2021-11-29 17:03:37 -0500

    cpp - removed spurious debugging output

--- src/situatedSIPP/structs.h
+++ src/situatedSIPP/structs.h
@@ -170,18 +170,10 @@ struct RTResultPathInfo:  public ResultPathInfo{
       rpi.reopened = reopened;
       rpi.reexpanded = reexpanded;
       rpi.path = rtn2nl(path);
-      DEBUG_MSG_RED("pathfound");
-      DEBUG_MSG_RED(pathfound);
       for (RTResultPathInfo rtrpi: iterationPath){
         rpi.iterationPath.push_back(rtrpi.toRPI());
       }
-      //ResultPathInfo::iterationPath = rtn2n(iterationPath);
-      //DEBUG_MSG_RED("RTSECTIONS");
-      //DEBUG_MSG_RED(sections.size());
       rpi.sections = rtn2nv(sections);
-      DEBUG_MSG_RED("Section lengths");
-      DEBUG_MSG_RED(rpi.sections.size());
-      //DEBUG_MSG_RED(ResultPathInfo::sections.size());
       rpi.reexpanded_list = rtn2nl(reexpanded_list);
       return rpi;
     }
