2313c161a8e2d71bbcf7695f57371f337cc05698
Devin Thomas <devin.thomas@unh.edu>
2022-01-12 17:01:34 -0500

    cpp - implementing subintervals

--- src/situatedSIPP/subintervals.cpp
+++ src/situatedSIPP/subintervals.cpp
@@ -1,14 +1,29 @@
 #include "subintervals.h"
 #include <algorithm>
 #include <numeric>
-SubIntervals::SubIntervals(double _start, double _end, const std::vector<SubIntervals>& children): start(_start), end(_end){
-    auto total_length = std::accumulate(children.begin(), children.end(), 0, [](auto acc, const SubIntervals& c){return acc + c.size();});
-    auto child_intervals = std::vector<SubInterval>(total_length);
-    for(const auto& child: children){
-        for(auto it = child.relevant_start(_end); it != child.subintervals_end();it++){//this is wrong, fix.
 
+void SubIntervals::add(const SubInterval &si, double cost){
+    double new_start = si.start  - cost;
+    double new_end = si.end - cost;
+    double new_h = si.h + cost;
+    if ((new_h <= h(new_start))||(new_h <= h(new_end))){ // check for strong dominance
+        SubInterval new_si = SubInterval(new_start, new_end, new_h);
+        auto location = subintervals.lower_bound(new_si);
+        while (location != subintervals.end()){
+            if (new_si.strongly_dominates(*location)){
+                location = subintervals.erase(location);
+            }
+            else{
+                location++;
+            }
         }
+        subintervals.insert(new_si);
     }
-    std::sort(child_intervals.begin(), child_intervals.end(), std::greater<>());
-    const SubInterval& incumbant = SubInterval()
 }
+
+
+SubIntervals::SubIntervals(double _start, double _end, const std::vector<SubIntervals>& children, const std::vector<double>& children_costs): start(_start), end(_end){
+    
+}
+
+
--- src/situatedSIPP/subintervals.h
+++ src/situatedSIPP/subintervals.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <boost/container/flat_map.hpp>
+#include <set>
+#include <map>
 #include <limits>
 #include <vector>
-#include <pair>
+#include <utility>
+	
 
 struct SubInterval{
     double start;
@@ -10,7 +13,14 @@ struct SubInterval{
     double h;
     SubInterval(double _start, double _end, double _h):start(_start),end(_end),h(_h){}
     bool operator>(const SubInterval& right) const{
-       return (end > right.end);
+        return (end > right.end);
+    }
+    bool operator<(const SubInterval& right) const{
+        return (end < right.end);
+    }
+
+    bool inline strongly_dominates(const SubInterval& other) const{
+        return (other.h > ht(other.end)) && (other.h > ht(other.start));
     }
 
     double ht(double t){
@@ -28,12 +38,16 @@ class SubIntervals{
     private:
         double start;
         double end;
-        boost::container::flat_map<double, SubInterval, std::greater<>> subintervals;
+        std::set<SubInterval, std::greater<>> subintervals;
+        std::map<double, SubInterval> dominant;
+        void update_dominant();
     public:
-        SubIntervals(double _start, double _end, const std::vector<SubIntervals>& children);
-        SubIntervals(double _start, double _end, double h) : start(_start), end(_end){
-            subintervals.emplace(_end, SubInterval(start, _end, h));
-        }
+        SubIntervals(double _start, double _end, double _h):start(_start),end(_end){
+            subintervals.emplace(_start, _end, _h);
+            dominant.emplace(_end, _start, _end, _h);
+        };
+        SubIntervals(double _start, double _end, const std::vector<SubIntervals>& children, const std::vector<double>& children_cost);
+        void add(const SubInterval & si, double cost);
         double h(double t) const;
         auto inline size() const{
             return subintervals.size();
